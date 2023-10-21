165fac603d8882d175c971c297720769410740c4
Flavio Barboza <flavio.barboza@alchemysystems.com>
2023-10-20 19:57:03 -0600

    screen container component

---
+++ src/components/ScreenContainer.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+export default function ScreenContainer({
+  children,
+}: {
+  children: React.ReactNode;
+}): React.JSX.Element {
+  return <View style={styles.root}>{children}</View>;
+}
+
+const styles = StyleSheet.create({
+  root: {
+    display: 'flex',
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignContent: 'center',
+    paddingTop: 10,
+    paddingHorizontal: 10,
+  },
+});
