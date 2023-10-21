fc424d0363a6a3370e248037e4d07d34e2ee0fad
Flavio Barboza <flavio.barboza@alchemysystems.com>
2023-10-20 19:46:45 -0600

    theme file

---
+++ src/assets/theme.ts
@@ -0,0 +1,12 @@
+import { MD3LightTheme } from 'react-native-paper';
+
+export const theme = {
+  ...MD3LightTheme,
+  roundness: 1,
+  colors: {
+    ...MD3LightTheme.colors,
+    primary: '#274658',
+    secondary: '#ad5586',
+    tertiary: '#008ca1',
+  },
+};
