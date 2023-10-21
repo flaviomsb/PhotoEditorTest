865747d6b0254346a05cd39267b3d94d9106a6a6
Flavio Barboza <flavio.barboza@alchemysystems.com>
2023-10-20 19:55:54 -0600

    vehicle info inspections

--- src/services/db/populateDb.ts
+++ src/services/db/populateDb.ts
@@ -1,17 +1,27 @@
 import Realm from 'realm';
+import { faker } from '@faker-js/faker';
 import { realmConfig } from '../../models';
-import { users, inspections } from './data';
+
+function createUser() {
+  return {
+    id: faker.number.int({ min: 1, max: 500 }),
+    name: faker.person.fullName(),
+  };
+}
+
+const users = faker.helpers.multiple(createUser, { count: 30 });
+
+function createInspection() {
+  return {
+    id: new Realm.BSON.UUID(),
+    title: `${faker.vehicle.vehicle()} Inspection`,
+  };
+}
+
+const inspections = faker.helpers.multiple(createInspection, { count: 30 });
 
 export default async function populateDb() {
   const realm = await Realm.open(realmConfig);
-
-  if (realm.objects('User').length > 0) {
-    return;
-  }
-
-  function id() {
-    return new Realm.BSON.UUID();
-  }
 
   try {
     realm.write(() => {
@@ -19,7 +29,7 @@ export default async function populateDb() {
         const user = realm.create('User', rawUser, Realm.UpdateMode.Modified);
         realm.create(
           'Inspection',
-          { id: id(), ...inspections[index], user },
+          { ...inspections[index], user },
           Realm.UpdateMode.Modified,
         );
       });
