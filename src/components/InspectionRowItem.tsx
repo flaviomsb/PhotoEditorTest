39cda0e84b5222dff7bc8582ce5470a84a9e1d54
Flavio Barboza <flavio.barboza@alchemysystems.com>
2023-10-27 10:13:41 -0600

    added signature navigation icon

--- src/components/InspectionRowItem.tsx
+++ src/components/InspectionRowItem.tsx
@@ -1,33 +1,38 @@
 import React from 'react';
-import {
-  StyleProp,
-  StyleSheet,
-  TouchableOpacity,
-  ViewStyle,
-} from 'react-native';
-import { List } from 'react-native-paper';
+import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import type { RootParamList } from '../Routes';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
+import { IconButton, List } from 'react-native-paper';
 import type { Inspection } from '../models/Inspection';
 import DefectCount from './DefectCount';
-
-const leftIcon = (props: { color?: string; style?: StyleProp<ViewStyle> }) => (
-  <List.Icon {...props} icon="car-info" />
-);
 
 export default function InspectionRowItem({
   inspection,
   onPress,
+  navigation,
 }: {
   inspection: Inspection;
   onPress: () => void;
+  navigation: NativeStackNavigationProp<RootParamList, 'Home', undefined>;
 }) {
+  const signInspection = React.useCallback(() => {
+    navigation.push('InspectionSignature', {
+      id: inspection.id.toString(),
+    });
+  }, [inspection.id, navigation]);
+
   return (
     <TouchableOpacity style={styles.root} onPress={onPress}>
       <List.Item
         title={inspection.title}
         description={`Inspector: ${inspection.user.name}`}
-        left={leftIcon}
       />
-      <DefectCount count={inspection.defects?.length ?? 0} />
+      <View style={styles.icons}>
+        {inspection.signaturePath && (
+          <IconButton icon="file-sign" size={20} onPress={signInspection} />
+        )}
+        <DefectCount count={inspection.defects?.length ?? 0} />
+      </View>
     </TouchableOpacity>
   );
 }
@@ -36,6 +41,10 @@ const styles = StyleSheet.create({
   root: {
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  icons: {
+    flexDirection: 'row',
     alignItems: 'center',
   },
 });
